558a00347dcfeb44a6e044e45bb2e3d981c60e0f
felip <felip@pc4060>
2024-09-05 16:15:54 -0400

    Agregar galeria.java, sala.java, obra.java y modificar main

--- src/main/java/com/felipe/galeriadearte/GaleriaDeArte.java
+++ src/main/java/com/felipe/galeriadearte/GaleriaDeArte.java
@@ -1,8 +1,28 @@
 package com.felipe.galeriadearte;
 
-public class GaleriaDeArte {
+import java.io.*;
 
+public class GaleriaDeArte {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        Galeria galeria = new Galeria();
+        
+        Sala sala1 = new Sala("Sala 1");
+        sala1.agregarObra(new Obra("a", "autor a", 2000, 25000));
+        sala1.agregarObra(new Obra("b", "autor b", 2020, 259000));
+        
+        Sala sala2 = new Sala("Sala 2");
+        sala2.agregarObra(new Obra("c", "autor c", 1987, 25098));
+        sala2.agregarObra(new Obra("d", "autor d", 1992, 259700));
+        
+        Sala sala3 = new Sala("Sala 3");
+        sala3.agregarObra(new Obra("e", "autor e", 1886, 250898));
+        sala3.agregarObra(new Obra("f", "autor f", 1925, 259260));
+        
+        galeria.agregarSala(sala1);
+        galeria.agregarSala(sala2);        
+        galeria.agregarSala(sala3);
+        
+        
     }
 }
